525cd42d4f68cadf841bac8235bb6176272346bd
Leonardo Nunes <leonardonunes1314@gmail.com>
2024-07-29 19:38:19 -0300

    finish commit

--- script.js
+++ script.js
@@ -7,21 +7,30 @@ const screen2 = document
 const btnTry = document
 .querySelector('#btnTry')
 
+const inputNumber = document.
+querySelector('#inputNumber')
+
 const btnReset = document
 .querySelector('#btnReset')
+
+const tryText = document
+.querySelector('.tryText')
+
+
+
 
 
 const randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
+console.log(randomNumber)
 
 
 //função callback
+
 function handleTryClick(event) {
   event.preventDefault()
 
-  const inputNumber = document.querySelector('#inputNumber')
-
-  if (Number(inputNumber.value) == randomNumber) {
+  if (Number(inputNumber.value) === randomNumber) {
     screen1.classList.add('hide')
     screen2.classList.remove('hide')
 
@@ -29,15 +38,28 @@ function handleTryClick(event) {
     .querySelector('.screen2 h2')
     .innerText = `Acertou em ${xAttempts} tentativas`
   }
+  
+  tryText.classList.remove('hide')
   inputNumber.value = "";
   xAttempts++
+  
+  
 }
 
 
-//Eventos
 
+ 
+
+
+
+
+//events
+inputNumber.oninput = () =>{
+  tryText.classList.add('hide')
+}
 
 btnTry.addEventListener('click',handleTryClick)
+
 btnReset.addEventListener('click',function(){ 
 
   
